abad6e7a500f031237e5eb3073cfd6086f7c26e7
mairoo <jonghwa@pincoin.co.kr>
2023-10-09 19:55:04 +0900

    tailwind css support

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import './index.css'
+import './assets/tailwind.css'
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
-root.render(<h1>Hello React!</h1>)
+root.render(<h1 className="text-2xl font-bold text-red-600">Hello React!</h1>)
