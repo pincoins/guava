bc0f070c1a3f91787c67a5f6d5b14777288beb9c
mairoo <jonghwa@pincoin.co.kr>
2023-11-11 21:56:28 +0900

    createAsyncThunk type inference

--- src/store/thunks/fetchCategories.ts
+++ src/store/thunks/fetchCategories.ts
@@ -4,14 +4,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 // 어떤 데이터를 가져와서 어떤 데이터를 반환해야 하는 지 모르므로 리듀서 함수 생성 불가
 // `createSlice`의 `extraReducers`로 등록
 
-const fetchCategories = createAsyncThunk<
-  string[],
-  void,
-  { rejectValue: string }
->('categories/fetchCategories', async () => {
-  const response = { data: ['banana', 'apple'] };
+const fetchCategories = createAsyncThunk(
+  'categories/fetchCategories',
+  async () => {
+    const response = { data: ['banana', 'apple'] };
 
-  return response.data;
-});
+    return response.data;
+  }
+);
 
 export { fetchCategories };
