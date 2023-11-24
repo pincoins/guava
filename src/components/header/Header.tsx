504e251eb0d2e74a3a5f623b7ea5d3d37cddc6d0
mairoo <jonghwa@pincoin.co.kr>
2023-11-25 02:43:49 +0900

    mobile menu conditional rendering

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import NavbarMobile from './NavbarMobile';
 import NavbarDesktop from './NavbarDesktop';
 import className from 'classnames';
+import { useAppSelector } from '../../hooks/rtk-hooks';
+import { RootState } from '../../store';
 
 const Header = ({ ...rest }) => {
   // 데스크탑 메뉴: fluid (기본값)
   // 모바일 메뉴: fixed = ContainerFixed
 
+  const { isMobile } = useAppSelector((state: RootState) => state.viewport);
+
   const classes = className(rest.className, '');
 
   return (
     <header className={classes}>
-      <NavbarMobile className="shadow px-2 py-3" />
-      <NavbarDesktop className="" />
+      {isMobile && <NavbarMobile className="shadow px-2 py-3" />}
+      {!isMobile && <NavbarDesktop className="" />}
     </header>
   );
 };
