27b25bfb34201f04cf2ed2ee9fee5c3f89882f03
mairoo <jonghwa@pincoin.co.kr>
2023-11-28 01:07:57 +0900

    drawer item

--- src/components/header/Drawer.tsx
+++ src/components/header/Drawer.tsx
@@ -5,9 +5,9 @@ import { RootState } from '../../store';
 import { Dialog, Transition } from '@headlessui/react';
 import { Link } from 'react-router-dom';
 import {
-  MdCardGiftcard,
   MdInfoOutline,
   MdLogin,
+  MdOutlineArrowRight,
   MdOutlineMenu,
   MdPerson,
   MdSendToMobile,
@@ -20,31 +20,31 @@ const products = [
     id: 1,
     title: '구글기프트카드',
     to: '/shop/categories/구글',
-    icon: MdCardGiftcard,
+    icon: MdOutlineArrowRight,
   },
   {
     id: 2,
-    title: '구글기프트카드',
-    to: '/shop/categories/구글',
-    icon: MdCardGiftcard,
+    title: '넥슨카드',
+    to: '/shop/categories/넥슨',
+    icon: MdOutlineArrowRight,
   },
   {
     id: 3,
-    title: '구글기프트카드',
-    to: '/shop/categories/구글',
-    icon: MdCardGiftcard,
+    title: '해피머니',
+    to: '/shop/categories/해피',
+    icon: MdOutlineArrowRight,
   },
   {
     id: 4,
-    title: '구글기프트카드',
-    to: '/shop/categories/구글',
-    icon: MdCardGiftcard,
+    title: '컬쳐랜드상품권',
+    to: '/shop/categories/컬쳐',
+    icon: MdOutlineArrowRight,
   },
   {
     id: 5,
-    title: '구글기프트카드',
-    to: '/shop/categories/구글',
-    icon: MdCardGiftcard,
+    title: '문화상품권',
+    to: '/shop/categories/문화',
+    icon: MdOutlineArrowRight,
   },
 ];
 
@@ -185,14 +185,6 @@ const Drawer = ({
                       </Link>
                     );
                   })}
-                  <Link
-                    to="/"
-                    className="inline-flex gap-x-2 items-center border-b px-3 py-1"
-                    onClick={onClose}
-                  >
-                    <MdCardGiftcard />
-                    카드
-                  </Link>
                 </div>
                 <DrawerHeading>{window.location.hostname}</DrawerHeading>
               </Dialog.Panel>
