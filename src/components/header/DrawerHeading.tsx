7ea8a4d5ce43cac13b7f517194461f1958b697e6
mairoo <jonghwa@pincoin.co.kr>
2023-11-28 00:05:30 +0900

    drawer heading

---
+++ src/components/header/DrawerHeading.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+const DrawerHeading = ({ children }: { children?: ReactNode }) => {
+  return (
+    <div>
+      <h1 className="font-bold border-l-4 border-b border-green-600 bg-green-50 text-green-800 px-2 py-1">
+        {children}
+      </h1>
+    </div>
+  );
+};
+
+export default DrawerHeading;
