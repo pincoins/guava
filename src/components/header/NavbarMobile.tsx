b831d38dd9eafa3ac687c5fb13b6d937a1892668
mairoo <jonghwa@pincoin.co.kr>
2023-12-18 23:39:46 +0900

    mobile navbar

--- src/components/header/NavbarMobile.tsx
+++ src/components/header/NavbarMobile.tsx
@@ -48,6 +48,7 @@ const NavbarMobile = ({ ...rest }) => {
               장바구니
               <span className="inline-flex items-center justify-center px-1 text-sm text-white bg-[#e88f2f] rounded-full">
                 {items.length} {loginState}
+                {loginState === ('AUTHENTICATED' || 'EXPIRED') ? '1' : '0'}
               </span>
             </Link>
             {loginState === 'UNAUTHENTICATED' && (
