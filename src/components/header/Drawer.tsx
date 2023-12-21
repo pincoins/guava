e85693d76b7c00c907adec9f3321ee9688bfdf78
mairoo <jonghwa@pincoin.co.kr>
2023-12-21 11:05:06 +0900

    fab is open condition fix

--- src/components/header/Drawer.tsx
+++ src/components/header/Drawer.tsx
@@ -63,7 +63,7 @@ const Drawer = ({
     <>
       <Button
         onClick={onOpen}
-        className="text-2xl bg-cyan-500 text-white w-14 h-14 inline-flex justify-center items-center shadow-lg"
+        className="text-2xl bg-teal-500 text-white w-14 h-14 inline-flex justify-center items-center shadow-lg"
         rounded="full"
         flat
       >
--- src/components/header/Fab.tsx
+++ src/components/header/Fab.tsx
@@ -14,15 +14,19 @@ import {
   MdSendToMobile,
   MdShoppingBag,
 } from 'react-icons/md';
+import className from 'classnames';
 
 const Fab = ({ isOpen, toggle }: { isOpen: boolean; toggle: () => void }) => {
   const { loginState } = useAppSelector((state: RootState) => state.auth);
+
+  const defaultButtonClass =
+    'absolute text-sm bg-emerald-400 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg';
 
   return (
     <>
       <Button
         onClick={toggle}
-        className="text-2xl bg-cyan-300 text-white w-14 h-14 inline-flex justify-center items-center"
+        className="text-2xl bg-teal-300 text-white w-14 h-14 inline-flex justify-center items-center"
         rounded="full"
         flat
       >
@@ -31,44 +35,49 @@ const Fab = ({ isOpen, toggle }: { isOpen: boolean; toggle: () => void }) => {
       </Button>
 
       <div>
-        {(loginState === 'AUTHENTICATED' || loginState === 'EXPIRED') && (
-          <Transition
-            show={isOpen}
-            enter="transition ease-in-out duration-75 transform"
-            enterFrom="translate-y-[115px]"
-            enterTo="translate-y-0"
-            leave="transition ease-in-out duration-700 transform"
-            leaveFrom="translate-y-0"
-            leaveTo="translate-y-[115px]"
+        <Transition
+          show={
+            (loginState === 'AUTHENTICATED' || loginState === 'EXPIRED') &&
+            isOpen
+          }
+          enter="transition ease-in-out duration-75 transform"
+          enterFrom="translate-y-[115px]"
+          enterTo="translate-y-0"
+          leave="transition ease-in-out duration-700 transform"
+          leaveFrom="translate-y-0"
+          leaveTo="translate-y-[115px]"
+        >
+          <Link
+            to="/auth/profile"
+            onClick={toggle}
+            className={className(
+              defaultButtonClass,
+              'bottom-[125px] right-[10px]'
+            )}
           >
-            <Link
-              to="/auth/profile"
-              onClick={toggle}
-              className="absolute text-sm bg-sky-500 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg bottom-[125px] right-[10px]"
-            >
-              <MdOutlineSettings />
-            </Link>
-          </Transition>
-        )}
-        {loginState === 'UNAUTHENTICATED' && (
-          <Transition
-            show={isOpen}
-            enter="transition ease-in-out duration-75 transform"
-            enterFrom="translate-y-[115px]"
-            enterTo="translate-y-0"
-            leave="transition ease-in-out duration-700 transform"
-            leaveFrom="translate-y-0"
-            leaveTo="translate-y-[115px]"
+            <MdOutlineSettings />
+          </Link>
+        </Transition>
+        <Transition
+          show={loginState === 'UNAUTHENTICATED' && isOpen}
+          enter="transition ease-in-out duration-75 transform"
+          enterFrom="translate-y-[115px]"
+          enterTo="translate-y-0"
+          leave="transition ease-in-out duration-700 transform"
+          leaveFrom="translate-y-0"
+          leaveTo="translate-y-[115px]"
+        >
+          <Link
+            to="/auth/sign-in"
+            onClick={toggle}
+            className={className(
+              defaultButtonClass,
+              'bottom-[125px] right-[10px]'
+            )}
           >
-            <Link
-              to="/auth/sign-in"
-              onClick={toggle}
-              className="absolute text-sm bg-sky-500 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg bottom-[125px] right-[10px]"
-            >
-              <MdLogin />
-            </Link>
-          </Transition>
-        )}
+            <MdLogin />
+          </Link>
+        </Transition>
         <Transition
           show={isOpen}
           enter="transition ease-in-out duration-150 transform"
@@ -81,7 +90,10 @@ const Fab = ({ isOpen, toggle }: { isOpen: boolean; toggle: () => void }) => {
           <Link
             to="/shop/cart"
             onClick={toggle}
-            className="absolute text-sm bg-sky-500 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg bottom-[116px] right-[54px]"
+            className={className(
+              defaultButtonClass,
+              'bottom-[116px] right-[54px]'
+            )}
           >
             <MdShoppingBag />
           </Link>
@@ -98,7 +110,10 @@ const Fab = ({ isOpen, toggle }: { isOpen: boolean; toggle: () => void }) => {
           <Link
             to="/shop/orders"
             onClick={toggle}
-            className="absolute text-sm bg-sky-500 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg bottom-[91px] right-[91px]"
+            className={className(
+              defaultButtonClass,
+              'bottom-[91px] right-[91px]'
+            )}
           >
             <MdSendToMobile />
           </Link>
@@ -115,49 +130,58 @@ const Fab = ({ isOpen, toggle }: { isOpen: boolean; toggle: () => void }) => {
           <Link
             to="/help/faq"
             onClick={toggle}
-            className="absolute text-sm bg-sky-500 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg bottom-[54px] right-[116px]"
+            className={className(
+              defaultButtonClass,
+              'bottom-[54px] right-[116px]'
+            )}
           >
             <MdInfoOutline />
           </Link>
         </Transition>
-        {(loginState === 'AUTHENTICATED' || loginState === 'EXPIRED') && (
-          <Transition
-            show={isOpen}
-            enter="transition ease-in-out duration-700 transform"
-            enterFrom="translate-x-[115px]"
-            enterTo="-translate-x-0"
-            leave="transition ease-in-out duration-75 transform"
-            leaveFrom="-translate-x-0"
-            leaveTo="translate-x-[115px]"
+
+        <Transition
+          show={
+            (loginState === 'AUTHENTICATED' || loginState === 'EXPIRED') &&
+            isOpen
+          }
+          enter="transition ease-in-out duration-700 transform"
+          enterFrom="translate-x-[115px]"
+          enterTo="-translate-x-0"
+          leave="transition ease-in-out duration-75 transform"
+          leaveFrom="-translate-x-0"
+          leaveTo="translate-x-[115px]"
+        >
+          <Link
+            to="/auth/sign-out"
+            onClick={toggle}
+            className={className(
+              defaultButtonClass,
+              'bottom-[10px] right-[125px]'
+            )}
           >
-            <Link
-              to="/auth/sign-out"
-              onClick={toggle}
-              className="absolute text-sm bg-sky-500 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg bottom-[10px] right-[125px]"
-            >
-              <MdLogout />
-            </Link>
-          </Transition>
-        )}
-        {loginState === 'UNAUTHENTICATED' && (
-          <Transition
-            show={isOpen}
-            enter="transition ease-in-out duration-700 transform"
-            enterFrom="translate-x-[115px]"
-            enterTo="-translate-x-0"
-            leave="transition ease-in-out duration-75 transform"
-            leaveFrom="-translate-x-0"
-            leaveTo="translate-x-[115px]"
+            <MdLogout />
+          </Link>
+        </Transition>
+        <Transition
+          show={loginState === 'UNAUTHENTICATED' && isOpen}
+          enter="transition ease-in-out duration-700 transform"
+          enterFrom="translate-x-[115px]"
+          enterTo="-translate-x-0"
+          leave="transition ease-in-out duration-75 transform"
+          leaveFrom="-translate-x-0"
+          leaveTo="translate-x-[115px]"
+        >
+          <Link
+            to="/auth/sign-up"
+            onClick={toggle}
+            className={className(
+              defaultButtonClass,
+              'bottom-[10px] right-[125px]'
+            )}
           >
-            <Link
-              to="/auth/sign-up"
-              onClick={toggle}
-              className="absolute text-sm bg-sky-500 text-black w-9 h-9 inline-flex justify-center items-center rounded-full shadow-lg bottom-[10px] right-[125px]"
-            >
-              <MdPersonAdd />
-            </Link>
-          </Transition>
-        )}
+            <MdPersonAdd />
+          </Link>
+        </Transition>
       </div>
     </>
   );
