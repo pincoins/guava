1cab111bf0ea5a46142470e95c724a10d586507e
mairoo <jonghwa@pincoin.co.kr>
2023-12-26 18:50:37 +0900

    no swipe

--- src/components/carousel/CategoryCarousel.tsx
+++ src/components/carousel/CategoryCarousel.tsx
@@ -1,4 +1,4 @@
-import { TouchEventHandler, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Category } from '../../types';
 import { Link } from 'react-router-dom';
 import {
@@ -27,78 +27,7 @@ const CategoryCarousel = ({ categories }: { categories: Category[] }) => {
   const BLOCK_SIZE = isMobile ? 2 : 6;
   const NUMBER_OF_SLIDES = Math.ceil(categories.length / BLOCK_SIZE);
 
-  let touchStartX: number;
-  let touchEndX: number;
-
-  useEffect(() => {
-    const slides = [];
-
-    if (categories.length !== 0 && NUMBER_OF_SLIDES !== 0) {
-      for (let i = 0; i < NUMBER_OF_SLIDES; i++) {
-        slides.push(categories.slice(i * BLOCK_SIZE, (i + 1) * BLOCK_SIZE));
-      }
-      // 컴포넌트 렌더링 시점에 넘겨 받은 카테고리로 새 리스트를 구성
-      // 첫 레코드 앞에 끝 레코드, 끝 레코드 앞에 첫 레코드 삽입하여 자연스러운 순환 가능
-      setCurrentList([slides[NUMBER_OF_SLIDES - 1], ...slides, slides[0]]);
-
-      if (!AUTO_PLAY) return;
-      const interval = setInterval(next, INTERVAL_LENGTH);
-      return () => clearInterval(interval);
-    }
-  }, [categories, BLOCK_SIZE, NUMBER_OF_SLIDES, AUTO_PLAY]);
-
-  useEffect(() => {
-    setCarouselTransform(`translateX(-${currentIndex * 100}%)`); // 애니메이션 효과: x축 이동
-  }, [currentIndex]);
-
-  const moveToNthSlide = (index: number) => {
-    // 트랜지션이 일어나기 기다렸다가 종료되면 바로 트랜지션 없애고 실제 슬라이드로 바로 이동
-    setTimeout(() => {
-      setCurrentIndex(index);
-
-      setCarouselTransition(''); // 애니메이션 효과: ease-in-out 트랜지션 없애기
-    }, 150);
-  };
-
-  const handleMove = (direction: number) => {
-    const newIndex = currentIndex + direction; // +1: next / -1: prev
-
-    if (newIndex === NUMBER_OF_SLIDES + 1) {
-      moveToNthSlide(1);
-    } else if (newIndex === 0) {
-      moveToNthSlide(NUMBER_OF_SLIDES);
-    }
-
-    setCurrentIndex((prev) => prev + direction);
-
-    setCarouselTransition('transform 150ms ease-in-out'); // 애니메이션 효과: ease-in-out 트랜지션 설정
-  };
-
-  const handleTouchStart: TouchEventHandler<HTMLDivElement> = (e) => {
-    touchStartX = e.nativeEvent.touches[0].clientX;
-  };
-
-  const handleTouchMove: TouchEventHandler<HTMLDivElement> = (e) => {
-    const currTouchX = e.nativeEvent.changedTouches[0].clientX;
-
-    setCarouselTransform(
-      `translateX(calc(-${currentIndex}00% - ${
-        (touchStartX - currTouchX) * 2 || 0
-      }px))`
-    );
-  };
-
-  const handleTouchEnd: TouchEventHandler<HTMLDivElement> = (e) => {
-    touchEndX = e.nativeEvent.changedTouches[0].clientX;
-
-    if (touchStartX >= touchEndX) {
-      handleMove(1);
-    } else {
-      handleMove(-1);
-    }
-  };
-
-  const next = () => {
+  const next = useCallback(() => {
     if (currentIndex + 1 === NUMBER_OF_SLIDES + 1) {
       setTimeout(() => {
         setCurrentIndex(1);
@@ -115,6 +44,50 @@ const CategoryCarousel = ({ categories }: { categories: Category[] }) => {
     });
 
     setCarouselTransition('transform 150ms ease-in-out'); // 애니메이션 효과: ease-in-out 트랜지션 설정
+  }, [NUMBER_OF_SLIDES, currentIndex]);
+
+  const moveToNthSlide = (index: number) => {
+    // 트랜지션이 일어나기 기다렸다가 종료되면 바로 트랜지션 없애고 실제 슬라이드로 바로 이동
+    setTimeout(() => {
+      setCurrentIndex(index);
+
+      setCarouselTransition(''); // 애니메이션 효과: ease-in-out 트랜지션 없애기
+    }, 150);
+  };
+
+  useEffect(() => {
+    const slides = [];
+
+    if (categories.length !== 0 && NUMBER_OF_SLIDES !== 0) {
+      for (let i = 0; i < NUMBER_OF_SLIDES; i++) {
+        slides.push(categories.slice(i * BLOCK_SIZE, (i + 1) * BLOCK_SIZE));
+      }
+      // 컴포넌트 렌더링 시점에 넘겨 받은 카테고리로 새 리스트를 구성
+      // 첫 레코드 앞에 끝 레코드, 끝 레코드 앞에 첫 레코드 삽입하여 자연스러운 순환 가능
+      setCurrentList([slides[NUMBER_OF_SLIDES - 1], ...slides, slides[0]]);
+
+      if (!AUTO_PLAY) return;
+      const interval = setInterval(next, INTERVAL_LENGTH);
+      return () => clearInterval(interval);
+    }
+  }, [categories, BLOCK_SIZE, NUMBER_OF_SLIDES, AUTO_PLAY, next]);
+
+  useEffect(() => {
+    setCarouselTransform(`translateX(-${currentIndex * 100}%)`); // 애니메이션 효과: x축 이동
+  }, [currentIndex]);
+
+  const handleMove = (direction: number) => {
+    const newIndex = currentIndex + direction; // +1: next / -1: prev
+
+    if (newIndex === NUMBER_OF_SLIDES + 1) {
+      moveToNthSlide(1);
+    } else if (newIndex === 0) {
+      moveToNthSlide(NUMBER_OF_SLIDES);
+    }
+
+    setCurrentIndex((prev) => prev + direction);
+
+    setCarouselTransition('transform 150ms ease-in-out'); // 애니메이션 효과: ease-in-out 트랜지션 설정
   };
 
   return (
@@ -122,9 +95,6 @@ const CategoryCarousel = ({ categories }: { categories: Category[] }) => {
       <div className="overflow-hidden">
         <div
           className={'flex'}
-          onTouchStart={handleTouchStart}
-          onTouchMove={handleTouchMove}
-          onTouchEnd={handleTouchEnd}
           style={{
             transform: carouselTransform,
             transition: carouselTransition,
